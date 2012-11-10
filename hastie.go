edcccff407f60522158207044368088f6ec2cceb
tetleyt <tetleyt@yahoo.com>
2012-11-10 06:36:32 +0000

    Fix Limit out of bounds error.

--- hastie.go
+++ hastie.go
@@ -51,12 +51,17 @@ type Page struct {
 
 type PagesSlice []Page
 
-func (p PagesSlice) Get(i int) Page         { return p[i] }
-func (p PagesSlice) Len() int               { return len(p) }
-func (p PagesSlice) Less(i, j int) bool     { return p[i].Date.Unix() < p[j].Date.Unix() }
-func (p PagesSlice) Swap(i, j int)          { p[i], p[j] = p[j], p[i] }
-func (p PagesSlice) Sort()                  { sort.Sort(p) }
-func (p PagesSlice) Limit(n int) PagesSlice { return p[0:n] }
+func (p PagesSlice) Get(i int) Page     { return p[i] }
+func (p PagesSlice) Len() int           { return len(p) }
+func (p PagesSlice) Less(i, j int) bool { return p[i].Date.Unix() < p[j].Date.Unix() }
+func (p PagesSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p PagesSlice) Sort()              { sort.Sort(p) }
+func (p PagesSlice) Limit(n int) PagesSlice {
+	if n < len(p) {
+		return p[0:n]
+	}
+	return p
+}
 
 type CategoryList map[string]PagesSlice
 
