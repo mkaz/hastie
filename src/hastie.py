ff1580ac4945a84689412b2e9755ca064a9cac42
Marcus Kazmierczak <marcus@mkaz.com>
2023-02-08 09:03:40 -0800

    Basic working version, writing HTML :dance:

--- src/hastie.py
+++ src/hastie.py
@@ -2,8 +2,10 @@
 
 from config import init_args
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+# internal imports
 from page import get_page
-from templates import what_template
+from templates import *
 
 
 def main():
@@ -25,8 +27,11 @@ def main():
 
         tpl = jinja.get_template(tpl_name)
         html = tpl.render(page=page)
-        print(html)
-        print("--------------------------")
+        outfile = get_output_file(filename, args["content_dir"], args["output_dir"])
+
+        # create directories if they don't exist
+        outfile.parent.mkdir(exist_ok=True, parents=True)
+        outfile.write_text(html)
 
 
 if __name__ == "__main__":
