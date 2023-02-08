d7b4ed8268b3a7d97c3e84c317456c5193e87ace
Marcus Kazmierczak <marcus@mkaz.com>
2023-02-08 09:03:40 -0800

    Basic working version, writing HTML :dance:

--- src/templates.py
+++ src/templates.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 
 def what_template(filename: os.PathLike, content_dir: os.PathLike) -> str:
@@ -9,3 +10,15 @@ def what_template(filename: os.PathLike, content_dir: os.PathLike) -> str:
         return "category.html"
 
     return "page.html"
+
+
+def get_output_file(f: os.PathLike, c: os.PathLike, o: os.PathLike) -> os.PathLike:
+    """Takes filename, content directory, output directory and returns output file"""
+    # get file relative to content directory
+    jf = Path(os.path.relpath(f, start=c))
+
+    # switch markdown to html
+    jf = jf.with_suffix(".html")
+
+    # return relative file joined with output directory
+    return Path(o, jf)
