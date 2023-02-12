fef8f1cc29a092617e0a72850aa0676bf3c0b22e
Marcus Kazmierczak <marcus@mkaz.com>
2023-02-12 06:19:35 -0800

    Wrong side of issue

--- tests/test_hfs.py
+++ tests/test_hfs.py
@@ -1,14 +1,14 @@
 """Hastie file system module"""
 
 from pathlib import Path
-import hastie.hfs as hfs
+import hastie
 
 
 def test_get_output_dir_index():
     content_dir = Path("/pages")
     output_dir = Path("/output")
     filepath = Path("/pages/index.md")
-    p = hfs.get_output_file(filepath, content_dir, output_dir)
+    p = hatie.hfs.get_output_file(filepath, content_dir, output_dir)
     assert p == Path("/output/index.html")
 
 
@@ -16,7 +16,7 @@ def test_get_output_dir_page():
     content_dir = Path("/pages")
     output_dir = Path("/output")
     filepath = Path("/pages/getting-started.md")
-    p = hfs.get_output_file(filepath, content_dir, output_dir)
+    p = hastie.hfs.get_output_file(filepath, content_dir, output_dir)
     assert p == Path("/output/getting-started.html")
 
 
@@ -24,7 +24,7 @@ def test_get_output_dir_category_index():
     content_dir = Path("/pages")
     output_dir = Path("/output")
     filepath = Path("/pages/pastas/index.md")
-    p = hfs.get_output_file(filepath, content_dir, output_dir)
+    p = hastie.hfs.get_output_file(filepath, content_dir, output_dir)
     assert p == Path("/output/pastas/index.html")
 
 
@@ -32,5 +32,5 @@ def test_get_output_dir_category_page():
     content_dir = Path("/pages")
     output_dir = Path("/output")
     filepath = Path("/pages/pastas/macaroni.md")
-    p = hfs.get_output_file(filepath, content_dir, output_dir)
+    p = hastie.hfs.get_output_file(filepath, content_dir, output_dir)
     assert p == Path("/output/pastas/macaroni.html")
